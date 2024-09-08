3103e8848fb7dd55f7bee829952fd2e1bf496457
ebruaeo <ebruaeo@hotmail.com>
2024-09-08 17:42:49 +0300

    init commit and designs

---
+++ app/src/main/java/com/example/sleeptracker/sleeptracker/SleepTrackerFragment.kt
@@ -0,0 +1,37 @@
+package com.example.sleeptracker.sleeptracker
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.sleeptracker.R
+import com.example.sleeptracker.databinding.FragmentSleepTrackerBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [SleepTrackerFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class SleepTrackerFragment : Fragment() {
+    /**
+     * Called when the Fragment is ready to display content to the screen.
+     *
+     * This function uses DataBindingUtil to inflate R.layout.fragment_sleep_quality.
+     */
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+
+        // Get a reference to the binding object and inflate the fragment views.
+        val binding: FragmentSleepTrackerBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_sleep_tracker, container, false)
+
+        return binding.root
+    }
+}
