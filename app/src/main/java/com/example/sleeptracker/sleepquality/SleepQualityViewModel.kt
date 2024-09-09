4de64e4ae5722d5ebcaa7b472adbdb01d11fda41
ebruaeo <ebruaeo@hotmail.com>
2024-09-09 14:52:34 +0300

    Recording Sleep Quality

--- app/src/main/java/com/example/sleeptracker/sleepquality/SleepQualityViewModel.kt
+++ app/src/main/java/com/example/sleeptracker/sleepquality/SleepQualityViewModel.kt
@@ -1,2 +1,45 @@
 package com.example.sleeptracker.sleepquality
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.sleeptracker.database.SleepDatabaseDao
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+
+class SleepQualityViewModel(
+    private val sleepNightKey: Long = 0L,
+    val database: SleepDatabaseDao
+) : ViewModel() {
+    private val viewModelJob = Job()
+
+    private val _navigateToSleepTracker = MutableLiveData<Boolean?>()
+    val navigateToSleepTracker: LiveData<Boolean?>
+        get() = _navigateToSleepTracker
+
+    fun doneNavigating(){
+        _navigateToSleepTracker.value = null
+    }
+
+    fun onSetSleepQuality(quality: Int){
+        viewModelScope.launch {
+            withContext(Dispatchers.IO){
+                val tonight = database.get(sleepNightKey) ?: return@withContext
+                tonight.sleepQuality = quality
+                database.update(tonight)
+            }
+            _navigateToSleepTracker.value = true
+        }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        viewModelJob.cancel()
+    }
+}
+
