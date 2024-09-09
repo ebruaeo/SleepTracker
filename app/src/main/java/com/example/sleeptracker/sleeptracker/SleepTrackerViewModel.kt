5d9c93305526154207bc411bb8a9641466130c6f
ebruaeo <ebruaeo@hotmail.com>
2024-09-09 14:57:25 +0300

    Define a variable, nights. Then getAllNights() from the database and assign to the nights variable:

--- app/src/main/java/com/example/sleeptracker/sleeptracker/SleepTrackerViewModel.kt
+++ app/src/main/java/com/example/sleeptracker/sleeptracker/SleepTrackerViewModel.kt
@@ -1,8 +1,18 @@
 package com.example.sleeptracker.sleeptracker
 
 import android.app.Application
+import android.service.autofill.Transformation
+
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.map
+import androidx.lifecycle.viewModelScope
 import com.example.sleeptracker.database.SleepDatabaseDao
+import com.example.sleeptracker.database.SleepNight
+import com.example.sleeptracker.formatNights
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
@@ -13,9 +23,12 @@ class SleepTrackerViewModel(
 ) : AndroidViewModel(application) {
 
     private var viewModelJob = Job()
+
     override fun onCleared() {
         super.onCleared()
         viewModelJob.cancel()
     }
+
     // private val uiScope = CoroutineScope(Dispatchers.Main + viewModelJob)
+    private val nights = database.getAllNights()
 }
