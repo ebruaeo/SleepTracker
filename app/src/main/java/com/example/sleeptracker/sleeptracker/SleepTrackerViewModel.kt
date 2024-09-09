7ae8aac2c764c5d8a4d315c4bda8483ef948def0
ebruaeo <ebruaeo@hotmail.com>
2024-09-09 14:55:30 +0300

    Define viewModelJob and assign it an instance of Job.

--- app/src/main/java/com/example/sleeptracker/sleeptracker/SleepTrackerViewModel.kt
+++ app/src/main/java/com/example/sleeptracker/sleeptracker/SleepTrackerViewModel.kt
@@ -3,9 +3,13 @@ package com.example.sleeptracker.sleeptracker
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import com.example.sleeptracker.database.SleepDatabaseDao
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class SleepTrackerViewModel(
     val database: SleepDatabaseDao,
     application: Application
 ) : AndroidViewModel(application) {
+    private var viewModelJob = Job()
 }
